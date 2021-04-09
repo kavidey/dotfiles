5f10f0fe926c46ebedd3f78f09ffff6dee6a8a18
Kavi Dey <kaviasher@gmail.com>
2021-04-09 16:21:53 -0700

    Update everything to check for admin privelage and clean up printouts

--- windows/config.ps1
+++ windows/config.ps1
@@ -3,8 +3,8 @@ $filepath = "C:\Users\$env:USERNAME\AppData\Roaming\Microsoft\Windows\Start Menu
 $filename = "autohotkey.lnk"
 if (-not(Test-Path -Path $filepath\$filename -PathType Container)) {
     try {
-        Write-Host "Creating symlinks to start AHK scrips on boot..."
-        New-Item -ItemType Junction -Path $filepath -Name $filename -Target {autohotkey} -Force | Out-Null
+        Write-Host "  Creating symlinks to start AHK scrips on boot..."
+        New-Item -ItemType SymbolicLink -Path $filepath -Name $filename -Target "$PSScriptRoot\autohotkey\main.ahk" -Force | Out-Null
     } catch {
         throw $_.Exception.Message
     }
