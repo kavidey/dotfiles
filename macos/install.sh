b49114a8639c72d76e96a59766898dc6ff49e910
Kavi Dey <kaviasher@gmail.com>
2021-09-26 23:35:07 -0700

    Clean up setup.sh

--- macos/install.sh
+++ macos/install.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
 echo "Installation script"
+
+### brew ###
+# Install brew
+#/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+### mas (Mac App Store) ###
+# Install mas
+brew install mas
--- macos/setup.sh
+++ macos/setup.sh
@@ -2,10 +2,12 @@
 
 xcode-select  --install
 
-read  -n 1 -p "Press [ENTER] once Xcode Developer Tools are finished installing:" foo
+read  -n 1 -p "Press any key once Command Line Developer Tools are finished installing: " foo
+echo ""
 
 git clone https://github.com/KaviMD/dotfiles.git
 echo "Cloned full dotfiles repo"
 cd ~/dotfiles/macos
 echo "Running installation script"
+chmod +x install.sh
 ./install.sh
